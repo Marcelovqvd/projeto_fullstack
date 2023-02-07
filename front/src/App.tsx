dccd1bf348ef93ef6f5cd272e171cf926d06f14b
Marcelo <marcelovqvd@gmail.com>
2023-02-06 22:55:03 -0300

    feat: routes and layout default

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,7 +1,9 @@
+import { Router } from "./Router";
+
 export function App() {
   return (
-      <div className="App">
-        <h1>Hello</h1>
+      <div>
+        <Router />
       </div>      
   )
 }
